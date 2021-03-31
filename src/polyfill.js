d6e7c575323e2fcebc5de17758916897c1d19525
Mike <kenr.mk@gmail.com>
2021-03-31 18:22:15 +0200

    fixed bug in polyfill

--- src/polyfill.js
+++ src/polyfill.js
@@ -2,16 +2,17 @@
 let dummy = document.createElement('div')
 let animation = dummy.animate({})
 let Animation = animation.constructor
-if ('finished' in Animation.prototype) return
-Object.defineProperty(Animation.prototype, 'finished', {
-	get() {
-		if (this._finished)
-			return this._finished
-		if (this.playState === 'finished')
-			return this._finished = new Promise.resolve()
-		return this._finished = new Promise((resolve, reject) => {
-			this.onfinish = resolve
-			this.oncancel = reject
-		})
-	}
-})
+if (!('finished' in Animation.prototype)) {
+	Object.defineProperty(Animation.prototype, 'finished', {
+		get() {
+			if (this._finished)
+				return this._finished
+			if (this.playState === 'finished')
+				return this._finished = new Promise.resolve()
+			return this._finished = new Promise((resolve, reject) => {
+				this.onfinish = resolve
+				this.oncancel = reject
+			})
+		}
+	})
+}
